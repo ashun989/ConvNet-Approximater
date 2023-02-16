f27d0a0d2aff3cb1a35e63ff912d0d5c44ac8d25
Quan-Sheng Zeng <zengqs@mail.nankai.edu.cn>
2023-02-16 18:42:57 +0800

    add support to profile

---
+++ configs/msca-rep/multi_layers/msca-rep_d1_ml10_fix_mscan-t.py
@@ -0,0 +1,8 @@
+_base_ = ['./msca-rep_d1_ml2_fix_mscan-t.py']
+
+filters = [
+    dict(
+        type="IndicesFilter",
+        indices=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    )
+]
---
+++ configs/msca-rep/multi_layers/msca-rep_d1_ml2_fix_mscan-t.py
@@ -0,0 +1,28 @@
+_base_ = ['../../_base_/models/mscan/mscan-t.py']
+
+app = dict(
+    type="MscaRep",
+    decomp=1,
+    fix=True
+)
+
+filters = [
+    dict(
+        type="IndicesFilter",
+        indices=(1,2)
+    )
+]
+
+hooks = [
+    dict(
+        type='ClassEvalHook',
+        priority=50,
+        eval_cfg=dict(
+            crop_pct=0.9,
+            mean=(0.5, 0.5, 0.5),
+            std=(0.5, 0.5, 0.5),
+            data='/Zalick/Datasets/ILSVRC2012/', )
+    )
+]
+
+output_name = 'mscan_t_d1_ml2_fix.pth'
